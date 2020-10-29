8a2af45e9c253c8981a76e8133113efafc0b4a02
danielfromberg <50125500+danielfromberg@users.noreply.github.com>
2020-10-28 22:44:05 -0500

    Add files via upload
    made a mistake

---
+++ client.086932f2bc24e534f82a.js
@@ -0,0 +1,226 @@
+/** ***************************************************************************************************************************\
+*                                                                                                                             *
+*                                                                                                                             *
+*                                                                                                                             *
+*                                                                .;ldxxo,                                                     *
+*                       ,;;;;;;;;;;cxkkx                      'dOkdox0NW0                                                     *
+*                     c:.           .NMM;                   :kl.                                                              *
+*                    .k   ;'        .NMMO                 ;k:                                                                 *
+*                     cl;lWo        xlKMM.              .xl                                                                   *
+*                       ..         :0 cMMo             'k.                                                                    *
+*                                 .X. .NMN.           :d     ,cO0O,'',;cx0O  .;d00d;.   .;d00d,.    ;d00o;.                   *
+*                                 0;   oMMo          cl        KMl       ,K'    XM,       ;WM'       .K'                      *
+*                                do    .NMW.        cc        :MK         .,    kM:      ':OM;      .l.                       *
+*                               ck      lMMk       ;l        .NM'     ';        oMo     ': dMl     .l                         *
+*                              ;0.       0MM:     'd         dMk....:k0         :Mk    ':  cMx    .l                          *
+*                             '0.        .WMW.   .k.        .WN'...'dM,         'M0   ':   ,M0   .l                           *
+*                        ....;X,....      :MMK.  x,.........0Mc     .x          .MN  ':    .MX  .l                            *
+*                       .';dWMMWo;'.       lMMO ;d.lXMMNxc,lM0       .    .,     XM.':      XM..c                             *
+*    'coxo,                l0MM0            oMMOX.  ;Nc   .NM'          .xo      OMl:       OMc:                              *
+*   x,  ,XW.             .kl OMM.            cMMX. ,x.  .;KMx    ..';lxXMd       oMc        dMc                               *
+*   ;                   :k.  ,MMk             ,NMXol   .,,,,,,,,,,,,,,,,'        .;         ',                                *
+*   l                 ,x:     0MM.             .OMWl      ';;,;;.       .'''',''','.       .'''''''   .''''''.                *
+*   ol             .co;       ,MMd             .d:0MK;.cko,.   .d0,      .kMN.    .:KO'     .xMMo..    .X0l'.                 *
+*    .lc:'.....':cc:.          0MW.           ;o   ,KMMK.        kM:      0Mc       :MK      OMo     .;:.                     *
+*        .,;:;,..              ;MMd          l;    :NNoKNd'      cMN     :M0        dMd     ;MK    ,;,                        *
+*                               KMW.       .d.    lMN.  .:xOxc'. oMW    .NW.      .dKc      XM, 'd:.                          *
+*                               :MMo      ,o     ,MM;        .;ccNMK,,,.dMO,,,cl:;:.       lM0;;KMx                           *
+*                                KMN.    c:      xM0            lMW'   'WN.   :MK.        .WW,  .KMd                          *
+*                                :MMl  .d'       kMx           ,WW:    KM:     :WK.       kMl    .0Md                         *
+*             .';;;;,.            XMN .o.        :MO          :WK'    cMO       :MK.     ;MK       kMk                        *
+*           :c;.    .;o.          cMMkc           lW:       'O0:     'NM,        :MK.   .NM:        lW0.                      *
+*         ,o.         dd           NN,             .ol;,,;:ll.    .;o0KKx;.       cKO:,cOKKk:'       'XN;                     *
+*        ,o          kW:          .d.                                                                  oWk.            oOdl,  *
+*        k.                      cc                                                                     .dNd.          :.  c  *
+*        O                     ,o.                                                                        .cOk;.           o  *
+*        :l                  .o;                                                                             .:ool:'....,;:.  *
+*         :o.              ;l;                                                                                    ..','..     *
+*          .cc:.      .,:cc.                                                                                                  *
+*             .,::::::,.                                                                                                      *
+*                                                                                                                             *
+*                                                                                                                             *
+*                                                     scripts/client.086932f2bc24e534f82a.js#c88cff54410d1c7086a3                                                      *
+*                                                            via                                                              *
+*                                                       client#505bc1b3990cd4771475                                                         *
+*                                                                                                                             *
+*                                           Code by New York, a Vox Media network.                                            *
+*                                 (c) 2020 Vox Media, Inc. All rights emphatically reserved.                                  *
+*                                                                                                                             *
+*                                                                                                                             *
+*                                                                                                                             *
+\*****************************************************************************************************************************/
+
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["client"],{
+
+/***/ 7649:
+/*!**************************!*\
+  !*** ./client/client.js ***!
+  \**************************/
+/*! namespace exports */
+/*! export initializeFosse [provided] [no usage info] [missing usage info prevents renaming] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__, __webpack_require__.n, __webpack_require__.r, __webpack_exports__, __webpack_require__.e, __webpack_require__.t, __webpack_require__.d, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "initializeFosse": () => /* binding */ initializeFosse
+/* harmony export */ });
+/* harmony import */ var core_js_modules_web_dom_collections_iterator_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! core-js/modules/web.dom-collections.iterator.js */ 4861);
+/* harmony import */ var core_js_modules_web_dom_collections_iterator_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_web_dom_collections_iterator_js__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/jordan/Desktop/nymag/choreographer/packages/fosse/src/client/client.js";
+
+const initializeFosse = async () => {
+  const [React, ReactDOM, {
+    Fosse
+  }] = await Promise.all([__webpack_require__.e(/*! import() */ "vendor-node_modules_react_index_js").then(__webpack_require__.t.bind(__webpack_require__, /*! react */ 2735, 3)), Promise.all(/*! import() */[__webpack_require__.e("vendor-node_modules_react_index_js"), __webpack_require__.e("vendor-node_modules_react-dom_index_js")]).then(__webpack_require__.t.bind(__webpack_require__, /*! react-dom */ 2788, 3)), Promise.all(/*! import() */[__webpack_require__.e("vendor-node_modules_core-js_internals_string-multibyte_js"), __webpack_require__.e("vendor-node_modules_common-tags_es_index_js-node_modules_core-js_modules_web_url_js-node_modu-851a85"), __webpack_require__.e("lib_index_js")]).then(__webpack_require__.bind(__webpack_require__, /*! ../lib */ 7658))]);
+  const fosse = new Fosse();
+  const [{
+    BabyGrowl,
+    CliffTakeover,
+    Reset
+  }, {
+    Egyptienne,
+    LibreFranklin,
+    MillerText
+  }] = await Promise.all([Promise.all(/*! import() */[__webpack_require__.e("vendor-node_modules_react_index_js"), __webpack_require__.e("vendor-node_modules_styled-components_dist_styled-components_browser_esm_js"), __webpack_require__.e("vendor-node_modules_core-js_internals_string-multibyte_js"), __webpack_require__.e("vendor-node_modules_chroma-js_chroma_js-node_modules_core-js_modules_es_string_replace_js-nod-6f14f5"), __webpack_require__.e("paywall-reader-interface_src_components_index_js")]).then(__webpack_require__.bind(__webpack_require__, /*! @nymag/paywall-reader-interface/components/index.js */ 2850)), Promise.all(/*! import() */[__webpack_require__.e("vendor-node_modules_react_index_js"), __webpack_require__.e("vendor-node_modules_styled-components_dist_styled-components_browser_esm_js"), __webpack_require__.e("paywall-reader-interface_src_assets_fonts_index_js")]).then(__webpack_require__.bind(__webpack_require__, /*! @nymag/paywall-reader-interface/assets/fonts/index.js */ 5060))]);
+
+  const ToutTree = ({
+    toutData,
+    toutType
+  }) => {
+    const onSignIn = React.useCallback(() => fosse.sendMessage('tout:sign-in', {
+      toutType
+    }), [toutType]);
+    const onDismiss = React.useCallback(() => fosse.sendMessage('tout:dismiss', {
+      toutType
+    }), [toutType]);
+    const onSubscribe = React.useCallback(() => fosse.sendMessage('tout:subscribe', {
+      toutType
+    }), [toutType]);
+    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Reset, {
+      __self: undefined,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 45,
+        columnNumber: 9
+      }
+    }), /*#__PURE__*/React.createElement(Egyptienne, {
+      __self: undefined,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 46,
+        columnNumber: 9
+      }
+    }), /*#__PURE__*/React.createElement(LibreFranklin, {
+      __self: undefined,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 47,
+        columnNumber: 9
+      }
+    }), /*#__PURE__*/React.createElement(MillerText, {
+      __self: undefined,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 48,
+        columnNumber: 9
+      }
+    }), toutType === 'baby-growl' && /*#__PURE__*/React.createElement(BabyGrowl, Object.assign({}, toutData, {
+      visible: toutType && toutType === 'baby-growl',
+      onDismiss: onDismiss,
+      onSubscribe: onSubscribe,
+      __self: undefined,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 50,
+        columnNumber: 11
+      }
+    })), (toutType === 'cliff-takeover' || toutType === 'warning-growl-2') && /*#__PURE__*/React.createElement(CliffTakeover, Object.assign({}, toutData, {
+      dismissible: toutType === 'warning-growl-2',
+      visible: toutType && (toutType === 'cliff-takeover' || toutType === 'warning-growl-2'),
+      onDismiss: onDismiss,
+      onSignIn: onSignIn,
+      onSubscribe: onSubscribe,
+      __self: undefined,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 59,
+        columnNumber: 11
+      }
+    })));
+  };
+
+  const main = document.querySelector('main');
+
+  const renderPaywall = ({
+    toutData,
+    toutType
+  } = {}) => {
+    ReactDOM.render( /*#__PURE__*/React.createElement(ToutTree, {
+      toutData: toutData,
+      toutType: toutType,
+      __self: undefined,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 77,
+        columnNumber: 21
+      }
+    }), main);
+    requestAnimationFrame(() => toutType && fosse.sendMessage('tout:visible', {
+      toutType
+    }));
+  };
+
+  document.addEventListener('load', () => {
+    renderPaywall();
+  }); // FIXME: This nested object stuff is a mess.
+
+  fosse.on('tout:show', ({
+    payload: {
+      toutData,
+      toutType
+    } = {}
+  } = {}) => {
+    renderPaywall({
+      toutData,
+      toutType
+    });
+  });
+  fosse.on('tout:hide', () => {
+    renderPaywall();
+  }); // Event to get the iframe content's height
+
+  fosse.on('frame:height', () => {
+    const currentHeight = document.querySelector('.tout').offsetHeight;
+    fosse.sendMessage('frame:height', {
+      currentHeight
+    });
+  });
+};
+
+/***/ }),
+
+/***/ 1984:
+/*!*************************!*\
+  !*** ./client/index.js ***!
+  \*************************/
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _client__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./client */ 7649);
+
+(0,_client__WEBPACK_IMPORTED_MODULE_0__.initializeFosse)().catch(err => {
+  throw err;
+});
+
+/***/ })
+
+},
+0,[[1984,"runtime","vendor-node_modules_core-js_modules_web_dom-collections_iterator_js"]]]);
+//# sourceMappingURL=https://fosse.nymag.com/fosse/v1.4.0/maps/scripts/client.086932f2bc24e534f82a.js.map
